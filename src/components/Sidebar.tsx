ec276568688a7254186c7fd8e8920ece329bc6eb
Breno Baptista <brenobaptista@protonmail.com>
2021-09-21 18:09:15 -0300

    Renamed Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
-const SideBar = (): JSX.Element => (
+const Sidebar = (): JSX.Element => (
   <nav className='w-64 h-screen p-6 bg-gray-300 dark:bg-gray-900' />
 )
 
-export default SideBar
+export default Sidebar
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import SideBar from '../components/SideBar'
+import Sidebar from '../components/Sidebar'
 import Wrapper from '../components/Wrapper'
 
 const Home = (): JSX.Element => (
   <div className='text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-700 font-sans antialiased'>
     <div className='flex'>
-      <SideBar />
+      <Sidebar />
       <Wrapper />
     </div>
   </div>
