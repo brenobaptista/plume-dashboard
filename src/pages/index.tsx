b5faeb54750e8f32c0edd26603f5a852931f1a6f
Breno Baptista <brenobaptista@protonmail.com>
2021-09-21 20:47:22 -0300

    Removed Content component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,21 @@
 import { useState } from 'react'
-import Content from '../components/Content'
+import Main from '../components/Main'
 import Sidebar from '../components/Sidebar'
 import SidebarContext from '../contexts/SidebarContext'
+import Toolbar from '../components/Toolbar'
 
 const Home = (): JSX.Element => {
   const [collapsed, setCollapsed] = useState(false)
 
   return (
-    <div className='text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-700 transition-colors font-sans antialiased'>
-      <div className='flex'>
-        <SidebarContext.Provider value={{ collapsed, setCollapsed }}>
-          <Sidebar />
-          <Content />
-        </SidebarContext.Provider>
-      </div>
+    <div className='flex text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-700 transition-colors font-sans antialiased'>
+      <SidebarContext.Provider value={{ collapsed, setCollapsed }}>
+        <Sidebar />
+        <div className='w-full'>
+          <Toolbar />
+          <Main />
+        </div>
+      </SidebarContext.Provider>
     </div>
   )
 }
