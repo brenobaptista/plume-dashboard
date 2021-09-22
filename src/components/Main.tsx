e6593af6ab5de53fdaaa2f94a53f28a0777f374f
Breno Baptista <brenobaptista@protonmail.com>
2021-09-22 00:13:50 -0300

    Updated Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,7 @@
-const Main = (): JSX.Element => <main className='container p-6' />
+const Main = (): JSX.Element => (
+  <main className='p-6 sm:p-10'>
+    <div className='w-full p-6 rounded bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700' />
+  </main>
+)
 
 export default Main
