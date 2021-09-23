3171a32d7726ecc6fc5b832223e885d3c6930422
Breno Baptista <brenobaptista@protonmail.com>
2021-09-22 21:20:49 -0300

    Added logo to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import Feather from './icons/Feather'
+
 interface Props {
   collapsed: boolean
 }
@@ -7,7 +9,12 @@ const Sidebar = ({ collapsed }: Props): JSX.Element => (
     className={`w-64 h-screen p-6 bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700 ${
       collapsed ? 'ml-0 sm:-ml-64' : '-ml-64 sm:ml-0'
     } transition-spacing duration-500 ease-in-out`}
-  />
+  >
+    <div className='flex space-x-3 justify-center text-xl font-bold'>
+      <Feather width={28} height={28} />
+      <span>Plume</span>
+    </div>
+  </nav>
 )
 
 export default Sidebar
