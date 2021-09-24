983eeca49092cd8de34ef7a54aed1c4c902fcd6b
Breno Baptista <brenobaptista@protonmail.com>
2021-09-23 23:40:30 -0300

    Added heading to Main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,6 @@
 const Main = (): JSX.Element => (
-  <main className='p-6 sm:p-10'>
+  <main className='py-6 px-6 sm:px-12'>
+    <h1 className='text-xl font-bold pb-4'>Dashboard</h1>
     <div className='w-full p-6 rounded bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700' />
   </main>
 )
