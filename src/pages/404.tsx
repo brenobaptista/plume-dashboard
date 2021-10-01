aaa158bbb592b3c7a02be9e27501d24b354e8203
Breno Baptista <brenobaptista@protonmail.com>
2021-09-30 23:14:47 -0300

    Added custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+import Theme from '../components/Theme'
+
+const Custom404 = (): JSX.Element => (
+  <>
+    <Head>
+      <title>404: Not found</title>
+    </Head>
+    <div className='flex items-center justify-around h-screen p-6'>
+      <div>
+        <h1 className='flex text-2xl font-bold tracking-wide'>
+          404: NOT FOUND
+          <Theme className='pl-8 hover:opacity-75' />
+        </h1>
+        <p className='max-w-md mt-4 mb-8 text-xl text-justify'>
+          It seems you have found something that used to exist or you spelled
+          something wrong.
+        </p>
+        <Link href='/'>
+          <a className='px-4 py-2 rounded hover:opacity-75 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700'>
+            Return to website
+          </a>
+        </Link>
+      </div>
+    </div>
+  </>
+)
+
+export default Custom404
