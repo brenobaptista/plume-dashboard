f132bf7e03afdc4a301a094ea9cd5ed30d3f91aa
Breno Baptista <brenobaptista@protonmail.com>
2021-10-07 08:05:13 -0300

    Added link to / in sidebar logo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import Feather from './icons/Feather'
 
 interface Props {
@@ -10,10 +12,14 @@ const Sidebar = ({ collapsed }: Props): JSX.Element => (
       collapsed ? 'ml-0 sm:-ml-64' : '-ml-64 sm:ml-0'
     } transition-spacing motion-reduce:transition-none duration-300 sm:duration-500 ease-in-out`}
   >
-    <div className='flex space-x-3 justify-center text-xl font-bold'>
-      <Feather width={28} height={28} />
-      <span>Plume</span>
-    </div>
+    <Link href='/'>
+      <a>
+        <div className='flex space-x-3 justify-center text-xl font-bold'>
+          <Feather width={28} height={28} />
+          <span>Plume</span>
+        </div>
+      </a>
+    </Link>
   </nav>
 )
 
