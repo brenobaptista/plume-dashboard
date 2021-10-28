b8343662073525e8d5eef54a035262a23741a1b6
Breno Baptista <brenobaptista@protonmail.com>
2021-10-27 22:51:37 -0300

    Added sign in page

---
+++ src/pages/signin.tsx
@@ -0,0 +1,91 @@
+import { useState } from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+
+import Eye from '../components/icons/Eye'
+import EyeOff from '../components/icons/EyeOff'
+import Feather from '../components/icons/Feather'
+import Layout from '../components/Layout'
+import Theme from '../components/Theme'
+
+const Dashboard = (): JSX.Element => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [showPassword, setShowPassword] = useState<boolean>(false)
+
+  const handleSignIn = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    /* submit email and password to back-end then redirect the user if they are correct */
+    /* otherwise, display an error dialog in browser */
+  }
+
+  return (
+    <Layout>
+      <Head>
+        <title>Plume - Sign In</title>
+      </Head>
+      <div className='text-right'>
+        <Theme className='hover:opacity-75' />
+      </div>
+      <div className='flex justify-center pb-2'>
+        <Feather width={40} height={40} />
+      </div>
+      <div className='flex justify-center pb-4 text-xl font-bold'>
+        Welcome to Plume
+      </div>
+      <form onSubmit={handleSignIn} className='flex-grow max-w-md space-y-4'>
+        <label className='block space-y-2'>
+          <span>Email</span>
+          <input
+            required
+            type='email'
+            placeholder='Email'
+            value={email}
+            onChange={event => setEmail(event.target.value.toLowerCase())}
+            className='w-full px-3 py-2 text-sm bg-gray-100 dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded placeholder-gray-400 dark:placeholder-gray-600'
+          />
+        </label>
+        <label className='block relative space-y-2'>
+          <span>Password</span>
+          <input
+            required
+            minLength={8}
+            autoComplete='current-password'
+            type={showPassword ? 'text' : 'password'}
+            placeholder='Password'
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+            className='w-full pl-3 pr-10 py-2 text-sm bg-gray-100 dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded placeholder-gray-400 dark:placeholder-gray-600'
+          />
+          <div
+            className='absolute top-6 right-0 px-3 py-2 text-gray-400 dark:text-gray-600 cursor-pointer'
+            onClick={() => setShowPassword(!showPassword)}
+            aria-hidden='true'
+          >
+            {showPassword ? (
+              <EyeOff width={20} height={20} />
+            ) : (
+              <Eye width={20} height={20} />
+            )}
+          </div>
+        </label>
+        <button
+          type='submit'
+          className='w-full text-sm px-4 py-2 bg-purple-400 dark:bg-purple-600 hover:opacity-75 rounded'
+        >
+          Sign In
+        </button>
+      </form>
+      <div className='flex justify-between pt-6 text-sm'>
+        <Link href='/reset-password'>
+          <a className='hover:opacity-75'>Reset Password</a>
+        </Link>
+        <Link href='/sign-up'>
+          <a className='hover:opacity-75'>Sign Up</a>
+        </Link>
+      </div>
+    </Layout>
+  )
+}
+
+export default Dashboard
