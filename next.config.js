eaed9ec0c4ce2bd5d0f6e31fd740d291333b53af
Breno Baptista <brenobaptista@protonmail.com>
2022-12-08 00:02:12 -0300

    Remove preact (to avoid bugs in production)

--- next.config.js
+++ next.config.js
@@ -11,18 +11,6 @@ module.exports = {
         headers: securityHeaders
       }
     ]
-  },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      })
-    }
-
-    return config
   }
 }
 
