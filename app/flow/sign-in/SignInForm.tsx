7422ef82c75fd9b0e6f3a58cb592888ec4d8bd56
Breno Baptista <brenobaptista@protonmail.com>
2023-08-04 22:29:37 -0300

    Implement InputPassword component

--- app/flow/sign-in/SignInForm.tsx
+++ app/flow/sign-in/SignInForm.tsx
@@ -2,12 +2,11 @@
 
 import { useState } from 'react'
 
-import { Eye, EyeOff } from '@/icons'
+import InputPassword from '@/components/InputPassword'
 
 const SignInForm = () => {
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
-  const [showPassword, setShowPassword] = useState<boolean>(false)
 
   const handleSignIn = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -30,30 +29,15 @@ const SignInForm = () => {
           className='w-full rounded border border-gray-200 bg-gray-100 px-3 py-2 text-sm placeholder-gray-400 dark:border-gray-700 dark:bg-gray-900 dark:placeholder-gray-600'
         />
       </label>
-      <label className='relative block space-y-2'>
-        <span>Password</span>
-        <input
-          required
-          minLength={8}
-          autoComplete='current-password'
-          type={showPassword ? 'text' : 'password'}
-          placeholder='Password'
-          value={password}
-          onChange={event => setPassword(event.target.value)}
-          className='w-full rounded border border-gray-200 bg-gray-100 py-2 pl-3 pr-10 text-sm placeholder-gray-400 dark:border-gray-700 dark:bg-gray-900 dark:placeholder-gray-600'
-        />
-        <div
-          className='absolute right-0 top-6 cursor-pointer px-3 py-2 text-gray-400 dark:text-gray-600'
-          onClick={() => setShowPassword(!showPassword)}
-          aria-hidden='true'
-        >
-          {showPassword ? (
-            <EyeOff width={20} height={20} />
-          ) : (
-            <Eye width={20} height={20} />
-          )}
-        </div>
-      </label>
+      <InputPassword
+        label='Password'
+        required
+        minLength={8}
+        autoComplete='current-password'
+        placeholder='Password'
+        value={password}
+        onChange={event => setPassword(event.target.value)}
+      />
       <button
         type='submit'
         className='w-full rounded bg-violet-400 px-4 py-2 text-sm font-medium hover:opacity-75 dark:bg-violet-600'
--- app/flow/sign-up/SignUpForm.tsx
+++ app/flow/sign-up/SignUpForm.tsx
@@ -2,13 +2,12 @@
 
 import { useState } from 'react'
 
-import { Eye, EyeOff } from '@/icons'
+import InputPassword from '@/components/InputPassword'
 
 const SignUpForm = () => {
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   const [confirmedPassword, setConfirmedPassword] = useState<string>('')
-  const [showPassword, setShowPassword] = useState<boolean>(false)
 
   const handleSignUp = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -33,56 +32,26 @@ const SignUpForm = () => {
           className='w-full rounded border border-gray-200 bg-gray-100 px-3 py-2 text-sm placeholder-gray-400 dark:border-gray-700 dark:bg-gray-900 dark:placeholder-gray-600'
         />
       </label>
-      <label className='relative block space-y-2'>
-        <span>Password</span>
-        <input
-          required
-          pattern='(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,24}'
-          title='Must contain at least one lowercase letter, one uppercase letter, one numeric value and one special symbol, and the total length should be between 8 and 24.'
-          autoComplete='new-password'
-          type={showPassword ? 'text' : 'password'}
-          placeholder='New password'
-          value={password}
-          onChange={event => setPassword(event.target.value)}
-          className='w-full rounded border border-gray-200 bg-gray-100 py-2 pl-3 pr-10 text-sm placeholder-gray-400 dark:border-gray-700 dark:bg-gray-900 dark:placeholder-gray-600'
-        />
-        <div
-          className='absolute right-0 top-6 cursor-pointer px-3 py-2 text-gray-400 dark:text-gray-600'
-          onClick={() => setShowPassword(!showPassword)}
-          aria-hidden='true'
-        >
-          {showPassword ? (
-            <EyeOff width={20} height={20} />
-          ) : (
-            <Eye width={20} height={20} />
-          )}
-        </div>
-      </label>
-      <label className='relative block space-y-2'>
-        <span>Confirm password</span>
-        <input
-          required
-          pattern='(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,24}'
-          title='Must contain at least one lowercase letter, one uppercase letter, one numeric value and one special symbol, and the total length should be between 8 and 24.'
-          autoComplete='new-password'
-          type={showPassword ? 'text' : 'password'}
-          placeholder='Confirm password'
-          value={confirmedPassword}
-          onChange={event => setConfirmedPassword(event.target.value)}
-          className='w-full rounded border border-gray-200 bg-gray-100 py-2 pl-3 pr-10 text-sm placeholder-gray-400 dark:border-gray-700 dark:bg-gray-900 dark:placeholder-gray-600'
-        />
-        <div
-          className='absolute right-0 top-6 cursor-pointer px-3 py-2 text-gray-400 dark:text-gray-600'
-          onClick={() => setShowPassword(!showPassword)}
-          aria-hidden='true'
-        >
-          {showPassword ? (
-            <EyeOff width={20} height={20} />
-          ) : (
-            <Eye width={20} height={20} />
-          )}
-        </div>
-      </label>
+      <InputPassword
+        label='Password'
+        required
+        pattern='(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,24}'
+        title='Must contain at least one lowercase letter, one uppercase letter, one numeric value and one special symbol, and the total length should be between 8 and 24.'
+        autoComplete='new-password'
+        placeholder='Password'
+        value={password}
+        onChange={event => setPassword(event.target.value)}
+      />
+      <InputPassword
+        label='Confirm password'
+        required
+        pattern='(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,24}'
+        title='Must contain at least one lowercase letter, one uppercase letter, one numeric value and one special symbol, and the total length should be between 8 and 24.'
+        autoComplete='new-password'
+        placeholder='Confirm password'
+        value={confirmedPassword}
+        onChange={event => setConfirmedPassword(event.target.value)}
+      />
       <button
         type='submit'
         className='w-full rounded bg-violet-400 px-4 py-2 text-sm font-medium hover:opacity-75 dark:bg-violet-600'
