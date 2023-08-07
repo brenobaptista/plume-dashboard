ad6948922bca75a71fe764efc48a7071c5241dad
Breno Baptista <brenobaptista@protonmail.com>
2023-08-07 00:17:40 -0300

    Fix logo position

--- app/dashboard/Sidebar.tsx
+++ app/dashboard/Sidebar.tsx
@@ -12,14 +12,16 @@ const Sidebar = forwardRef(
   ({ collapsed }: Props, sidebarRef: React.Ref<HTMLElement>) => (
     <nav
       ref={sidebarRef}
-      className={`min-h-screen w-64 border-r border-gray-200 bg-white p-6 dark:border-gray-700 dark:bg-gray-800 ${
+      className={`min-h-screen w-64 border-r border-gray-200 bg-white p-7 dark:border-gray-700 dark:bg-gray-800 ${
         collapsed ? 'ml-0 sm:-ml-64' : '-ml-64 sm:ml-0'
       } transition-spacing duration-300 ease-in-out motion-reduce:transition-none sm:duration-500`}
     >
       <Link href='/dashboard' aria-label='Go to the dashboard' passHref>
-        <div className='flex justify-center space-x-3'>
+        <div className='flex items-center justify-center space-x-3'>
           <Feather width={28} height={28} />
-          <Heading tag='h2'>Plume</Heading>
+          <Heading tag='h2' className='pb-0'>
+            Plume
+          </Heading>
         </div>
       </Link>
     </nav>
