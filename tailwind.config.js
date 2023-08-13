189259145a9931edb7966fddf08f4b2c1e04a1e7
Breno Baptista <brenobaptista@protonmail.com>
2023-08-13 20:46:16 -0300

    Remove unused assets for removed Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,6 @@ module.exports = {
         spacing: 'margin, padding',
         dropdown: 'opacity, transform'
       },
-      animation: {
-        beat: 'beat 1s ease-out infinite'
-      },
-      keyframes: {
-        beat: {
-          '0%, 100%': { transform: 'scale(1)' },
-          '25%': { transform: 'scale(1.2)' }
-        }
-      },
       fontFamily: {
         sans: ['var(--font-inter)', ...fontFamily.sans]
       }
